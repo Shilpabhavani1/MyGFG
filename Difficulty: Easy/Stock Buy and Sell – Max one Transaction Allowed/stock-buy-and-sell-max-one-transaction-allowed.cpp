9b1a15044b714506ffecddedbf01596b86aba896
Bhavani <96565325+Shilpabhavani1@users.noreply.github.com>
2024-11-22 09:06:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Stock Buy and Sell – Max one Transaction Allowed/stock-buy-and-sell-max-one-transaction-allowed.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int maximumProfit(vector<int> &prices) {
+        // code here
+        int max_profit=0;
+        int mini=prices[0];
+        
+        for(int i=0;i<prices.size();i++)
+        {
+           if(prices[i]<mini) mini=prices[i];
+           
+           max_profit=max(max_profit,prices[i]-mini);
+        }
+        
+        return max_profit;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> prices;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            prices.push_back(number);
+        }
+
+        Solution ob;
+        int ans = ob.maximumProfit(prices);
+        cout << ans << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
